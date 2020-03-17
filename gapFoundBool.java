a2a26675ce907889c9375113223afc1b4256b614
VT71 <56217276+VT71@users.noreply.github.com>
2020-03-17 12:45:57 +0000

    Add files via upload

---
+++ gapFoundBool.java
@@ -0,0 +1,15 @@
+package CS1821_Project;
+
+public class gapFoundBool{
+	private boolean bool;
+	
+	public gapFoundBool() {
+		this.bool = false;
+	}
+	public void setBool(boolean bool) {
+		this.bool=bool;
+	}
+	public boolean getBooel() {
+		return(this.bool);
+	}
+}
